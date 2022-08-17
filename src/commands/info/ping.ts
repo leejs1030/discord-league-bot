084fa5a186aad68d76d4892059abe8b6eced86e4
leejs1030 <leejs1030@korea.ac.kr>
2022-08-18 00:12:16 +0900

    eslint 적용

--- src/commands/info/ping.ts
+++ src/commands/info/ping.ts
@@ -1,9 +1,9 @@
-import { Command } from "../../structures/Command";
+import { Command } from '../../structures/Command';
 
 export default new Command({
-    name: "ping",
-    description: "replies with pong",
-    run: async ({ interaction }) => {
-        interaction.followUp("Pong3");
-    }
+  name: 'ping',
+  description: 'replies with pong',
+  run: async ({ interaction }) => {
+    interaction.followUp('Pong3');
+  },
 });
--- src/typings/Command.ts
+++ src/typings/Command.ts
@@ -1,11 +1,11 @@
 import {
-    ChatInputApplicationCommandData,
-    CommandInteraction,
-    CommandInteractionOptionResolver,
-    GuildMember,
-    PermissionResolvable
-} from "discord.js";
-import { ExtendedClient } from "../structures/Client";
+  ChatInputApplicationCommandData,
+  CommandInteraction,
+  CommandInteractionOptionResolver,
+  GuildMember,
+  PermissionResolvable,
+} from 'discord.js';
+import { ExtendedClient } from '../structures/Client';
 
 /**
  * {
@@ -17,18 +17,18 @@ import { ExtendedClient } from "../structures/Client";
  * }
  */
 export interface ExtendedInteraction extends CommandInteraction {
-    member: GuildMember;
+  member: GuildMember;
 }
 
 interface RunOptions {
-    client: ExtendedClient;
-    interaction: ExtendedInteraction;
-    args: CommandInteractionOptionResolver;
+  client: ExtendedClient;
+  interaction: ExtendedInteraction;
+  args: CommandInteractionOptionResolver;
 }
 
 type RunFunction = (options: RunOptions) => any;
 
 export type CommandType = {
-    userPermissions?: PermissionResolvable[];
-    run: RunFunction;
+  userPermissions?: PermissionResolvable[];
+  run: RunFunction;
 } & ChatInputApplicationCommandData;
