75f05b92d3f6f389b9a5de5cddbda75b1b363737
leejs1030 <leejs1030@korea.ac.kr>
2022-08-20 22:00:44 +0900

    랭크 전적 리팩토링

--- src/commands/league-of-legends/rank-history.ts
+++ src/commands/league-of-legends/rank-history.ts
@@ -1,16 +1,8 @@
 import { Command } from '../../structures/Command';
 import { ApplicationCommandOptionTypes } from 'discord.js/typings/enums';
 import { getSummonerByNickname } from '../../libs/riot/get-summoner-by-nickname';
-import { getTimeDiff } from '../../libs/utils/get-time-diff';
 import { getSoloRankHistory } from '../../libs/riot/get-solo-rank-history';
-
-const positionMapper = {
-  UTILITY: '서폿',
-  JUNGLE: '정글',
-  TOP: '탑',
-  MIDDLE: '미드',
-  BOTTOM: '원딜',
-};
+import { getShortInfoByGame } from '../../libs/riot/get-short-info-by-game';
 
 export default new Command({
   name: 'games',
@@ -21,22 +13,7 @@ export default new Command({
     const games = await getSoloRankHistory(puuid);
     const date = new Date();
 
-    const history = games.map((game) => {
-      const { gameCreation, participants, teams } = game.info;
-      const { championName, teamId, individualPosition } = participants.filter(
-        (participant) => participant.puuid === puuid,
-      )[0];
-      const duration =
-        participants.reduce((acc, cur) => {
-          return acc >= cur.timePlayed ? acc : cur.timePlayed;
-        }, -1) * 1000;
-      const win = teams.filter((team) => team.teamId === teamId)[0].win;
-      const endDate = new Date(duration + gameCreation);
-      const time = getTimeDiff(date, endDate);
-
-      const wl = win ? '승' : '패';
-      return `${championName}(${positionMapper[individualPosition]}), ${wl}, ${time}`;
-    });
+    const history = games.map((game) => getShortInfoByGame({ puuid, game, date }));
 
     const string = `${nickname}의 전적\n${history.join('\n')}`;
     return interaction.followUp(string);
---
+++ src/libs/riot/get-short-info-by-game.ts
@@ -0,0 +1,34 @@
+import { getTimeDiff } from '../utils/get-time-diff';
+
+const positionMapper = {
+  UTILITY: '서폿',
+  JUNGLE: '정글',
+  TOP: '탑',
+  MIDDLE: '미드',
+  BOTTOM: '원딜',
+};
+
+export const getShortInfoByGame = async ({
+  puuid,
+  game,
+  date,
+}: {
+  puuid: string;
+  game: any;
+  date: Date;
+}) => {
+  const { gameCreation, participants, teams } = game.info;
+  const { championName, teamId, individualPosition } = participants.filter(
+    (participant) => participant.puuid === puuid,
+  )[0];
+  const duration =
+    participants.reduce((acc, cur) => {
+      return acc >= cur.timePlayed ? acc : cur.timePlayed;
+    }, -1) * 1000;
+  const win = teams.filter((team) => team.teamId === teamId)[0].win;
+  const endDate = new Date(duration + gameCreation);
+  const time = getTimeDiff(date, endDate);
+
+  const wl = win ? '승' : '패';
+  return `${championName}(${positionMapper[individualPosition]}), ${wl}, ${time}`;
+};
