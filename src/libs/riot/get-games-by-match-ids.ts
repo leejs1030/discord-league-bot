4a981d16016b4b6ec6828c2fec35fccb0a32c94a
leejs1030 <leejs1030@korea.ac.kr>
2022-08-20 22:02:15 +0900

    console log 삭제

--- src/libs/riot/get-games-by-match-ids.ts
+++ src/libs/riot/get-games-by-match-ids.ts
@@ -3,7 +3,6 @@ import { headers } from './headers';
 
 const baseUrl = 'https://asia.api.riotgames.com/lol/match/v5/matches';
 export const getGamesByMatchIds = async (arr: string[]) => {
-  console.log('this?');
   return Promise.all(
     arr.map((matchId) => axios.get(`${baseUrl}/${matchId}`, { headers }).then((res) => res.data)),
   );
